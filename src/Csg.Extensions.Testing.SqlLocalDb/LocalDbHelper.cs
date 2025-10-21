d9931a9559e28201118efa08902fd2d20038e46c
Ryan Owens <rowens@architectnow.net>
2025-10-21 13:19:11 -0500

    feat: bump dotnet and nuget versions
     - bump to target net 8.0/9.0
     - bump nuget packages
     - replace system.data.sqlclient with microsoft.data.sqlclient
     - change C# version to default
     - enable nullability checks

--- src/Csg.Extensions.Testing.SqlLocalDb/LocalDbHelper.cs
+++ src/Csg.Extensions.Testing.SqlLocalDb/LocalDbHelper.cs
@@ -1,172 +1,185 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Text;
+using System.Runtime.InteropServices;
+using JetBrains.Annotations;
 
-namespace Csg.Extensions.Testing.SqlLocalDb
+namespace Csg.Extensions.Testing.SqlLocalDb;
+
+public static class LocalDbHelper
 {
-    public static class LocalDbHelper
+    public const string DataPath = @"%LOCALAPPDATA%\LocalDb";
+    private const string LocalDbExe = "sqllocaldb.exe";
+
+    [UsedImplicitly]
+    public static void CreateInstance(string instanceName)
     {
-        public static string DataPath = @"%LOCALAPPDATA%\LocalDb";
-        public static string LocalDbExe = "sqllocaldb.exe";
+        if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            throw new PlatformNotSupportedException();
+        
+        Exec(LocalDbExe, $"create {instanceName} -s");
+        var dataPath = System.IO.Path.Combine(Environment.ExpandEnvironmentVariables(DataPath), instanceName);
 
-        private static bool TryFindFirstPath(IEnumerable<string> paths, out string matchingPath)
+        if (!System.IO.Directory.Exists(dataPath))
         {
-            foreach (var path in paths)
-            {
-                string s = System.Environment.ExpandEnvironmentVariables(path);
-                if (System.IO.File.Exists(s))
-                {
-                    matchingPath = s;
-                    return true;
-                }
-            }
+            System.IO.Directory.CreateDirectory(dataPath);
+        }
+    }
 
-            matchingPath = null;
+    [UsedImplicitly]
+    public static string CreateDatabase(string dbName, string instanceName)
+    {
+        if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            throw new PlatformNotSupportedException();
+        
+        Exec("sqllocaldb.exe", $"create {instanceName} -s");
+
+        var connectionString = $"Data Source=(localdb)\\{instanceName};Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        var dataPath = System.IO.Path.Combine(Environment.ExpandEnvironmentVariables(DataPath), instanceName);
+
+        using var conn = new Microsoft.Data.SqlClient.SqlConnection(connectionString);
+        
+        conn.Open();
+        var cmd = conn.CreateCommand();
+        cmd.CommandText = $"""
+                           IF EXISTS (SELECT 1 FROM sys.databases WHERE [name]='{dbName}') 
+                           DROP DATABASE [{dbName}]; 
+                           CREATE DATABASE [{dbName}] 
+                           ON PRIMARY ( Name = {dbName}_data, FILENAME = '{dataPath}\{dbName}_data.mdf')
+                           LOG ON ( Name = {dbName}_log, FILENAME = '{dataPath}\{dbName}_log.ldf')
+                           ;
+                           """;
+
+        cmd.ExecuteNonQuery();
+        
+
+        connectionString = string.Concat(connectionString, $";Initial Catalog={dbName}");
+
+        return connectionString;
+    }
+    
+    [UsedImplicitly]
+    public static void DeleteDatabase(string databaseName, string instanceName)
+    {
+        if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            throw new PlatformNotSupportedException();
+        
+        var connectionString = $"Data Source=(localdb)\\{instanceName};Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+        using var conn = new Microsoft.Data.SqlClient.SqlConnection(connectionString);
+        conn.Open();
+        var cmd = conn.CreateCommand();
+        cmd.CommandText = $"""
+                           IF EXISTS (SELECT 1 FROM sys.databases WHERE [name]='{databaseName}') BEGIN
+                               ALTER DATABASE [{databaseName}] SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+                               DROP DATABASE [{databaseName}];
+                           END
+                           """;
+        cmd.ExecuteNonQuery();
+    }
+
+    [UsedImplicitly]
+    public static void StopInstance(string instanceName)
+    {
+        if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            throw new PlatformNotSupportedException();
+        
+        var resultCode = Exec(LocalDbExe, $"stop {instanceName} -k");
+
+        if (resultCode != 0)
+        {
+            throw new Exception($"sqllocaldb.exe stop result code {resultCode}");
+        }
+    }
+
+    [UsedImplicitly]
+    public static void DeleteInstance(string instanceName)
+    {
+        if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            throw new PlatformNotSupportedException();
+        
+        StopInstance(instanceName);
+
+        var resultCode = Exec(LocalDbExe, $"delete {instanceName}");
+        if (resultCode != 0)
+        {
+            throw new Exception($"sqllocaldb.exe delete result code {resultCode}");
+        }
+
+        var dataPath = System.IO.Path.Combine(Environment.ExpandEnvironmentVariables(DataPath), instanceName);
+        var files = System.IO.Directory.GetFiles(dataPath);
+        foreach (var file in files)
+        {
+            System.IO.File.Delete(file);
+        }
+    }
+
+    [UsedImplicitly]
+    public static bool TryDeleteInstance(string instanceName)
+    {
+        if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            throw new PlatformNotSupportedException();
+        
+        try
+        {
+            DeleteInstance(instanceName);
+            return true;
+        }
+        catch
+        {
             return false;
         }
+    }
+    
+    private static int Exec(string cmd, string arguments, bool useCmd = true, System.IO.StreamWriter? output = null)
+    {
+        var fileName = useCmd ? "cmd.exe" : cmd;
+        var args = useCmd ? string.Concat("/c ", cmd, " ",arguments): arguments;
 
-        private static int Exec(string cmd, string arguments, bool useCmd = true, System.IO.StreamWriter output = null)
+        if (output == null)
         {
-            string fileName = useCmd ? "cmd.exe" : cmd;
-            string args = useCmd ? string.Concat("/c ", cmd, " ",arguments): arguments;
-
-            if (output == null)
-            {
-                System.Diagnostics.Debug.WriteLine($"Executing {fileName} {args}");
-            }
-            else
-            {
-                output.WriteLine($"Executing {fileName} {args}");
-            }
-
-            var _processInfo = new ProcessStartInfo(fileName, args)
-            {
-                CreateNoWindow = true,
-                UseShellExecute = false,
-                RedirectStandardError = true,
-                RedirectStandardOutput = true
-            };
-
-            var _process = Process.Start(_processInfo);
-
-            _process.WaitForExit();
-
-            string _output = _process.StandardOutput.ReadToEnd();
-            string _error = _process.StandardError.ReadToEnd();
-
-            var _exitCode = _process.ExitCode;
-
-            if (output == null)
-            {
-                System.Diagnostics.Debug.WriteLine(_output);
-                System.Diagnostics.Debug.WriteLine(_error);
-            }
-            else
-            {
-                output.WriteLine(_output);
-                output.WriteLine(_error);
-            }
-
-            _process.Close();
-
-            return _exitCode;
+            Debug.WriteLine($"Executing {fileName} {args}");
+        }
+        else
+        {
+            output.WriteLine($"Executing {fileName} {args}");
         }
 
-        public static void CreateInstance(string instanceName)
+        var processInfo = new ProcessStartInfo(fileName, args)
         {
-            Exec(LocalDbExe, $"create {instanceName} -s");
-            string dataPath = System.IO.Path.Combine(Environment.ExpandEnvironmentVariables(DataPath), instanceName);
+            CreateNoWindow = true,
+            UseShellExecute = false,
+            RedirectStandardError = true,
+            RedirectStandardOutput = true
+        };
 
-            if (!System.IO.Directory.Exists(dataPath))
-            {
-                System.IO.Directory.CreateDirectory(dataPath);
-            }
+        var process = Process.Start(processInfo);
+
+        if (process is null)
+        {
+            Debug.WriteLine("Unable to Start Process");
+            return -1;
         }
 
-        public static string CreateDatabase(string dbName, string instanceName)
+        process.WaitForExit();
+
+        var processOutput = process.StandardOutput.ReadToEnd();
+        var error = process.StandardError.ReadToEnd();
+
+        var exitCode = process.ExitCode;
+
+        if (output == null)
         {
-            Exec("sqllocaldb.exe", $"create {instanceName} -s");
-
-            string connectionString = $"Data Source=(localdb)\\{instanceName};Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
-            string dataPath = System.IO.Path.Combine(Environment.ExpandEnvironmentVariables(DataPath), instanceName);
-            
-            using (var conn = new System.Data.SqlClient.SqlConnection(connectionString))
-            {
-                conn.Open();
-                var cmd = conn.CreateCommand();
-                cmd.CommandText = $@"
-IF EXISTS (SELECT 1 FROM sys.databases WHERE [name]='{dbName}') 
-DROP DATABASE [{dbName}]; 
-CREATE DATABASE [{dbName}] 
-ON PRIMARY ( Name = {dbName}_data, FILENAME = '{dataPath}\{dbName}_data.mdf')
-LOG ON ( Name = {dbName}_log, FILENAME = '{dataPath}\{dbName}_log.ldf')
-;";
-
-                cmd.ExecuteNonQuery();
-            }
-
-            connectionString = string.Concat(connectionString, $";Initial Catalog={dbName}");
-
-            return connectionString;
+            Debug.WriteLine(processOutput);
+            Debug.WriteLine(error);
         }
-                
-        public static void DeleteDatabase(string databaseName, string instanceName)
+        else
         {
-            string connectionString = $"Data Source=(localdb)\\{instanceName};Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
-
-            using (var conn = new System.Data.SqlClient.SqlConnection(connectionString))
-            {
-                conn.Open();
-                var cmd = conn.CreateCommand();
-                cmd.CommandText = $@"
-IF EXISTS (SELECT 1 FROM sys.databases WHERE [name]='{databaseName}') BEGIN
-    ALTER DATABASE [{databaseName}] SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
-    DROP DATABASE [{databaseName}];
-END";
-                cmd.ExecuteNonQuery();
-            }
+            output.WriteLine(processOutput);
+            output.WriteLine(error);
         }
 
-        public static void StopInstance(string instanceName)
-        {
-            int resultCode = Exec(LocalDbExe, $"stop {instanceName} -k");
+        process.Close();
 
-            if (resultCode != 0)
-            {
-                throw new Exception($"sqllocaldb.exe stop result code {resultCode}");
-            }
-        }
-
-        public static void DeleteInstance(string instanceName, bool deleteFiles = true)
-        {
-            StopInstance(instanceName);
-
-            var resultCode = Exec(LocalDbExe, $"delete {instanceName}");
-            if (resultCode != 0)
-            {
-                throw new Exception($"sqllocaldb.exe delete result code {resultCode}");
-            }
-
-            string dataPath = System.IO.Path.Combine(Environment.ExpandEnvironmentVariables(DataPath), instanceName);
-            var files = System.IO.Directory.GetFiles(dataPath);
-            foreach (var file in files)
-            {
-                System.IO.File.Delete(file);
-            }
-        }
-
-        public static bool TryDeleteInstance(string instanceName, bool deleteFiles = true)
-        {
-            try
-            {
-                DeleteInstance(instanceName, deleteFiles: deleteFiles);
-                return true;
-            }
-            catch
-            {
-                return false;
-            }
-        }
+        return exitCode;
     }
 }
--- tests/Csg.Extensions.Testing.SqlLocalDb.Tests/LocalDbHelperTests.cs
+++ tests/Csg.Extensions.Testing.SqlLocalDb.Tests/LocalDbHelperTests.cs
@@ -1,41 +1,44 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.IO;
+using JetBrains.Annotations;
 
-namespace Csg.Extensions.Testing.SqlLocalDb.Tests
+namespace Csg.Extensions.Testing.SqlLocalDb.Tests;
+
+[TestClass]
+[UsedImplicitly]
+public class LocalDbHelperTests
 {
-    [TestClass]
-    public class LocalDbHelperTests
+    private const string InstanceName = "UnitTestProject";
+
+    static LocalDbHelperTests()
     {
-        private static string InstanceName = "UnitTestProject";
-
-        static LocalDbHelperTests()
-        {
-            LocalDbHelper.TryDeleteInstance(InstanceName);
-            LocalDbHelper.CreateInstance(InstanceName);            
-        }      
+        LocalDbHelper.TryDeleteInstance(InstanceName);
+        LocalDbHelper.CreateInstance(InstanceName);            
+    }      
         
-        [AssemblyCleanup]
-        public static void Cleanup()
-        {
-            LocalDbHelper.DeleteInstance(InstanceName);
-        }
+    [AssemblyCleanup]
+    [UsedImplicitly]
+    public static void Cleanup()
+    {
+        LocalDbHelper.DeleteInstance(InstanceName);
+    }
  
-        [TestMethod]
-        public void TestCreateConnectCloseDeleteDatabase()
-        {
-            var connStr = LocalDbHelper.CreateDatabase("Database1", InstanceName);
+    [TestMethod]
+    [UsedImplicitly]
+    public void TestCreateConnectCloseDeleteDatabase()
+    {
+        var connStr = LocalDbHelper.CreateDatabase("Database1", InstanceName);
 
-            Assert.IsTrue(File.Exists(Environment.ExpandEnvironmentVariables(Path.Combine(LocalDbHelper.DataPath, InstanceName, "Database1_data.mdf"))));
+        Assert.IsTrue(File.Exists(Environment.ExpandEnvironmentVariables(Path.Combine(LocalDbHelper.DataPath, InstanceName, "Database1_data.mdf"))));
 
-            var conn = new System.Data.SqlClient.SqlConnection(connStr);
+        var conn = new Microsoft.Data.SqlClient.SqlConnection(connStr);
             
-            conn.Open();
-            Assert.AreEqual(System.Data.ConnectionState.Open, conn.State);
-            // intentionally leaving database connection open above 
+        conn.Open();
+        Assert.AreEqual(System.Data.ConnectionState.Open, conn.State);
+        // intentionally leaving database connection open above 
 
-            LocalDbHelper.DeleteDatabase("Database1", InstanceName);
-            Assert.IsFalse(File.Exists(Environment.ExpandEnvironmentVariables(Path.Combine(LocalDbHelper.DataPath, InstanceName, "Database1_data.mdf"))));
-        }
+        LocalDbHelper.DeleteDatabase("Database1", InstanceName);
+        Assert.IsFalse(File.Exists(Environment.ExpandEnvironmentVariables(Path.Combine(LocalDbHelper.DataPath, InstanceName, "Database1_data.mdf"))));
     }
 }
